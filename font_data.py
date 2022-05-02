27b9dd8289aea8e943e1ab4bdcfbd9bd564ea387
Nathan <62321581+nathanlafere@users.noreply.github.com>
2022-05-02 17:42:41 -0300

    Create font_data.py

---
+++ font_data.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.font.init()
+
+
+text_font = pygame.font.SysFont('Arial', 15)
+menu_font = pygame.font.SysFont('Arial', 15, True)
+obs_font = pygame.font.SysFont('Arial', 13)
+interface_font = pygame.font.SysFont('Arial', 15)
